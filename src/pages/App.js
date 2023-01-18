84f430ccc1c03b3835516a096786e57cf9bccd36
jcommaret@gmail.com <jcommaret@gmail.com>
2023-01-18 13:05:19 +0100

    Homepage update

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,14 +1,11 @@
-import logo from '../assets/img/logo.svg';
+import logo from '../assets/img/logo.png';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={logo} className="App-logo" alt="logo kasa" />
         <a
           className="App-link"
           href="https://reactjs.org"

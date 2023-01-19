b83d4465822940727e737cf6318146969250a36b
jcommaret@gmail.com <jcommaret@gmail.com>
2023-01-19 23:12:06 +0100

    ADDED : Logement List // Modifications

--- src/components/templates/Home.jsx
+++ src/components/templates/Home.jsx
@@ -1,7 +1,11 @@
 import Subheader from "./_partials/Subheader";
+import LogementsList from "./_partials/LogementsList";
 
 export default function Home() {
     return (
-      <Subheader/>   
+      <>
+        <Subheader/>
+        <LogementsList/>   
+      </>
     );
 }
---
+++ src/components/templates/_partials/LogementsList.jsx
@@ -0,0 +1,10 @@
+
+import '../../sass/logements_list.scss';
+
+export default function LogementsList() {
+    return (
+      <>
+        <div className="logements-list"></div>
+      </>
+    );
+}

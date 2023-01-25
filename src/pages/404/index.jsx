fc19a20248d135597ba6c04114b821d54409ae1b
jcommaret@gmail.com <jcommaret@gmail.com>
2023-01-25 11:53:51 +0100

    Put variables into 404 page

--- src/pages/404/index.jsx
+++ src/pages/404/index.jsx
@@ -3,15 +3,17 @@ import { Link } from "react-router-dom"
 import "./index.scss"
 
 export default function ErrorPage() {
+  const pageTitle = "404"
+  const pageDescription = "Oups ! La page que vous demandez n'existe pas."
+  const LinkToHome = "Retourner sur la page d’accueil"
+  
   return (
     <div className="NotFound">
       <div className="NotFound__content">
-        <h1 className="NotFound__title">404</h1>
-        <p className="NotFound__text">
-          Oups ! La page que vous demandez n'existe pas.
-        </p>
+        <h1 className="NotFound__title">{pageTitle}</h1>
+        <p className="NotFound__text">{pageDescription}</p>
         <Link to="/" className="NotFound__Link">
-          Retourner sur la page d’accueil
+          {LinkToHome}
         </Link>
       </div>
     </div>

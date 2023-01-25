f735c84a8fa74b626604d59d03426163c49e7738
jcommaret@gmail.com <jcommaret@gmail.com>
2023-01-25 20:09:20 +0100

    Prettier

--- src/components/LogementsList/index.jsx
+++ src/components/LogementsList/index.jsx
@@ -1,31 +1,30 @@
-
-import './index.scss';
+import "./index.scss"
 
 export default function LogementsList() {
-    return (
-      <>
-        <div className="logementsList">
-          <div className="logementsList-item">
-            <img className="image" src="" alt="plop" />
-          </div>
-
-          <div className="logementsList-item">
-            <img className="test" src="" alt="plop" />
-          </div>
-
-          <div className="logementsList-item">
-            <img className="test" src="" alt="plop" />
-          </div>
-
-          <div className="logementsList-item">
-            <img className="test" src="" alt="plop" />
-            <h2>Titre de la location</h2>
-          </div>
-
-          <div className="logementsList-item">
-            <img className="test" src="" alt="plop" />
-          </div>
+  return (
+    <>
+      <div className="logementsList">
+        <div className="logementsList-item">
+          <img className="image" src="" alt="plop" />
         </div>
-      </>
-    )
+
+        <div className="logementsList-item">
+          <img className="test" src="" alt="plop" />
+        </div>
+
+        <div className="logementsList-item">
+          <img className="test" src="" alt="plop" />
+        </div>
+
+        <div className="logementsList-item">
+          <img className="test" src="" alt="plop" />
+          <h2>Titre de la location</h2>
+        </div>
+
+        <div className="logementsList-item">
+          <img className="test" src="" alt="plop" />
+        </div>
+      </div>
+    </>
+  )
 }

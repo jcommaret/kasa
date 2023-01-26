c99096642eb9995a546088b8de93aa429c1c7f23
jcommaret@gmail.com <jcommaret@gmail.com>
2023-01-26 13:17:01 +0100

    About page

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,35 +1,12 @@
 import "./index.scss"
+import aboutItems from "./data.js"
+import Accordion from "../../components/Accordion"
 
 export default function About() {
-  const aboutItems = [
-    {
-      title: "Fiabilité",
-      description:
-        "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.",
-    },
-    {
-      title: "Respect",
-      description:
-        "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.",
-    },
-    {
-      title: "Service",
-      description:
-        "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.",
-    },
-    {
-      title: "Sécurité",
-      description:
-        "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
-    },
-  ]
   return (
     <div className="About">
-      {aboutItems.map((about, index) => (
-        <div key={index}>
-          <h2>{about.title}</h2>
-          <p>{about.description}</p>
-        </div>
+      {aboutItems.map(({ title, description }) => (
+        <Accordion title={title} content={description} />
       ))}
     </div>
   )

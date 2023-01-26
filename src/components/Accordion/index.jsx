94f7fe3ccadf841ae52a60aaa1e7f4f2788e664f
jcommaret@gmail.com <jcommaret@gmail.com>
2023-01-26 13:17:17 +0100

    Accordion

---
+++ src/components/Accordion/index.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react"
+import "./index.scss"
+
+const Accordion = ({ title, content }) => {
+  const [isActive, setIsActive] = useState(false)
+  return (
+    <div className="accordion">
+      <div className="accordion-title" onClick={() => setIsActive(!isActive)}>
+        <h2>{title}</h2>
+        {isActive ? (
+          <div className="close">Close</div>
+        ) : (
+          <div className="open">Open</div>
+        )}
+      </div>
+      {isActive && <div className="accordion-content">{content}</div>}
+    </div>
+  )
+}
+
+export default Accordion

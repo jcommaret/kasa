f406f4979c24c1199ff4c562e516219f35cf5c07
jcommaret@gmail.com <jcommaret@gmail.com>
2023-02-01 01:46:17 +0100

    About page design

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -2,7 +2,7 @@ import "./index.scss"
 import aboutItems from "./data.js"
 import Accordion from "../../components/Accordion"
 import ImageHeader from "../../components/ImageHeader"
-import backgroundImage from "../../assets/img/header/home.png"
+import backgroundImage from "../../assets/img/about/header-big.png"
 
 // Data
 const background = backgroundImage
@@ -11,11 +11,13 @@ const imageAlt = "Image de fond du header representant des vacances"
 export default function About() {
   return (
     <>
-      <ImageHeader background={background} imageAlt={imageAlt} />
       <div class="About">
-        {aboutItems.map(({ title, description }) => (
-          <Accordion title={title} content={description} />
-        ))}
+        <ImageHeader background={background} imageAlt={imageAlt} />
+        <div className="Accordion-list">
+          {aboutItems.map(({ title, content }) => (
+            <Accordion title={title} content={content} />
+          ))}
+        </div>
       </div>
     </>
   )

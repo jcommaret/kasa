92d29afdee5170334725d370e38faf46e8705b9d
jcommaret@gmail.com <jcommaret@gmail.com>
2023-02-07 12:15:19 +0100

    Slider : boutons and position

--- src/components/Slider/index.jsx
+++ src/components/Slider/index.jsx
@@ -3,8 +3,10 @@ import "./index.scss"
 const slideHeader = ({ text, background, imageAlt }) => {
   return (
     <div className="slideHeader">
+      <div className="slideHeader-prev"></div>
       <img className="slideHeader-image" src={background} alt={imageAlt} />
-      {text && <p className="imageHeader-text">{text}</p>}
+      {text && <p className="slideHeader-text">{text}</p>}
+      <div className="slideHeader-next"></div>
     </div>
   )
 }

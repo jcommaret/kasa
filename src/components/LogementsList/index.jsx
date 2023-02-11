1ab95df69ba1f92679bd5edfdeb350e845e54b90
jcommaret@gmail.com <jcommaret@gmail.com>
2023-02-11 18:49:01 +0100

    CSS : arrow position fixed

--- src/components/LogementsList/index.jsx
+++ src/components/LogementsList/index.jsx
@@ -4,24 +4,22 @@ import logements from "../../data/logements.json"
 export default function LogementsList() {
   const logementsList = logements
   return (
-    <>
-      <div className="logementsList">
-        {logementsList.map((logement, id) => (
-          <div key={id} className="logementsList-item">
-            <a href={`logement/${id}`}>
-              <div className="logementsList-item_details">
-                <img
-                  className="image"
-                  loading="lazy"
-                  src={logement.cover}
-                  alt="Visuel de l'appartement"
-                />
-                <h2>{logement.title}</h2>
-              </div>
-            </a>
-          </div>
-        ))}
-      </div>
-    </>
+    <div className="logementsList">
+      {logementsList.map((logement, id) => (
+        <div key={id} className="logementsList-item">
+          <a href={`logement/${id}`}>
+            <div className="logementsList-item_details">
+              <img
+                className="image"
+                loading="lazy"
+                src={logement.cover}
+                alt="Visuel de l'appartement"
+              />
+              <h2>{logement.title}</h2>
+            </div>
+          </a>
+        </div>
+      ))}
+    </div>
   )
 }

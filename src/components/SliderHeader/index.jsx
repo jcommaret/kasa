37d5505fd4fd3601b1ef75ecb90e46eb46e4c96d
jcommaret@gmail.com <jcommaret@gmail.com>
2023-02-13 12:19:14 +0100

    Numerotation and styles update

--- src/components/SliderHeader/index.jsx
+++ src/components/SliderHeader/index.jsx
@@ -1,31 +1,41 @@
 import "./index.scss"
 import { useState } from "react"
 
-export default function SliderHeader({ text, slides, imageAlt }) {
+export default function SliderHeader({ slides, imageAlt }) {
   const [currentSlide, setCurrentSlide] = useState(0)
+  const [currentNumber, setCurrentNumber] = useState(1)
 
   const goToPrev = () => {
     const isFirstSlide = currentSlide === 0
     const newIndex = isFirstSlide ? slides.length - 1 : currentSlide - 1
+    const isFirstNumber = currentNumber === 1
+    const newNumber = isFirstNumber ? slides.length : currentNumber - 1
     setCurrentSlide(newIndex)
+    setCurrentNumber(newNumber)
   }
 
   const goToNext = () => {
     const isLastSlide = currentSlide === slides.length - 1
     const newIndex = isLastSlide ? 0 : currentSlide + 1
+    const isLastNumber = currentNumber === slides.length
+    const newNumber = isLastNumber ? 1 : currentNumber + 1
     setCurrentSlide(newIndex)
+    setCurrentNumber(newNumber)
   }
 
   return (
-    <div className="slideHeader">
-      <div onClick={goToPrev} className="slideHeader-prev"></div>
+    <div className="sliderHeader">
+      <div onClick={goToPrev} className="sliderHeader-prev"></div>
       <img
-        className="slideHeader-image"
+        className="sliderHeader-image"
         src={slides[currentSlide]}
         alt={imageAlt}
       />
-      {text && <p className="slideHeader-text">{text}</p>}
-      <div onClick={goToNext} className="slideHeader-next"></div>
+      <p className="sliderHeader-text">
+        {[currentNumber]}/{slides.length}
+      </p>
+
+      <div onClick={goToNext} className="sliderHeader-next"></div>
     </div>
   )
 }

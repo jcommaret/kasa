20093694f782af5984346e0e5fcb16fa6d108cc9
jcommaret@gmail.com <jcommaret@gmail.com>
2023-02-13 11:38:36 +0100

    Features : Slider

--- src/components/SliderHeader/index.jsx
+++ src/components/SliderHeader/index.jsx
@@ -1,25 +1,31 @@
 import "./index.scss"
 import { useState } from "react"
 
-export default function SliderHeader({ text, background, imageAlt }) {
-  let [currentSlide] = useState(0)
+export default function SliderHeader({ text, slides, imageAlt }) {
+  const [currentSlide, setCurrentSlide] = useState(0)
+
+  const goToPrev = () => {
+    const isFirstSlide = currentSlide === 0
+    const newIndex = isFirstSlide ? slides.length - 1 : currentSlide - 1
+    setCurrentSlide(newIndex)
+  }
+
+  const goToNext = () => {
+    const isLastSlide = currentSlide === slides.length - 1
+    const newIndex = isLastSlide ? 0 : currentSlide + 1
+    setCurrentSlide(newIndex)
+  }
 
   return (
     <div className="slideHeader">
-      <div
-        //        onClick={ChangeSlide(background[currentSlide]-1)}
-        className="slideHeader-prev"
-      ></div>
+      <div onClick={goToPrev} className="slideHeader-prev"></div>
       <img
         className="slideHeader-image"
-        src={background[currentSlide]}
+        src={slides[currentSlide]}
         alt={imageAlt}
       />
       {text && <p className="slideHeader-text">{text}</p>}
-      <div
-        //      onClick={ChangeSlide(background[currentSlide]+1)}
-        className="slideHeader-next"
-      ></div>
+      <div onClick={goToNext} className="slideHeader-next"></div>
     </div>
   )
 }
